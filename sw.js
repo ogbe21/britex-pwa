199195624b3f7261c3f0580c276d6129d80168e2
Mauro3.3 <89676905+ogbe21@users.noreply.github.com>
2026-01-13 13:33:11 -0500

    Create new sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'britex-v1';
+const OFFLINE_URL = '/offline.html';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([OFFLINE_URL, '/']);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.match(OFFLINE_URL);
+      })
+    );
+  }
+});
